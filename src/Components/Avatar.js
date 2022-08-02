242981ddad405ab09dbffa2776ecfaf6d44f648f
MariahTali <talicuran.mariah@gmail.com>
2022-08-02 15:09:45 -0700

    added contributor images, working on css styling for each avatar being rendered in contributor file

--- src/Components/Avatar.js
+++ src/Components/Avatar.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import './styles/Avatar.css';
 
-function Avatar() {
+function Avatar(props) {
   return (
-    <div>
-      <>
-      
-      </>
+    <div className='avatar_container'>
+      <div className='footerContainer'>
+        <img src={props.image} />
+        <p>{props.name}</p>
+        <div className='avatarIcons'>
+          <a href={props.github}>
+            {<FaGithub style={{ color: 'whitesmoke' }} />}
+          </a>
+          <a href={props.linkedin}>
+            {<FaLinkedin style={{ color: 'whitesmoke' }} />}
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Avatar
+export default Avatar;
--- src/Components/Contributors.js
+++ src/Components/Contributors.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import './styles/Contributors.css'
+import React from 'react';
+import Avatar from './Avatar';
+import './styles/Contributors.css';
+import Gwen from '../images/gwen.png';
+import Mariah from '../images/mariah.png';
+import Jerald from '../images/jerald.png';
+import Josh from '../images/josh.png';
 
 function Contributors() {
   return (
-    <div className="contributors_body">
+    <>
+    <div className='contributors_h1'>
       <h1>Meet The Team</h1>
     </div>
-  )
+    <div className='contributor_container'>
+    <Avatar
+      image={Mariah}
+      name='Mariah Talicuran'
+      github='https://github.com/MariahTali'
+      linkedin='https://www.linkedin.com/in/mariahtalicuran/'
+    />
+  </div>
+  </>
+  );
 }
 
-export default Contributors
+export default Contributors;
